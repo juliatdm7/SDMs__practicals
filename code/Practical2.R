7141ba3a60476d3c32930c9022b359d37bf25a6b
juliatdm7 <juliatdm7@gmail.com>
2024-10-06 14:54:48 +0100

    First commit

---
+++ code/Practical2.R
@@ -0,0 +1,10 @@
+# Biodiversity Under Pressure - SDMs Practicals - Practical 2                     4/10/2024
+
+#Firstly, let's download all necessary packages and load them
+
+install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org")
+install.packages("predicts",dependencies=TRUE,repos="https://cloud.r-project.org")
+install.packages("terra",dependencies=TRUE,repos="https://cloud.r-project.org")
+library(geodata)
+library(predicts)
+library(terra)
